397d0e513d236a5b30b356347b6a1a6b71567c0b
Tushar Sharma <Tusharsharma20575@gmail.com>
2022-09-02 19:25:23 +0530

    Added homepage and navbar darkmode looking good

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 //import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import HomeForm from './components/HomeForm';
 // import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [toggleName, setToggleName] = useState('Enable Dark Mode');
+  const toggleMode =() =>{
+    if(mode === 'light'){
+      setMode('dark');
+      setToggleName("Enable Light Mode");
+      document.body.style.backgroundColor ='#042743';
+    }
+    else{
+      setMode('light');
+      setToggleName("Enable Dark Mode");
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title="My App" about="About" />
+      <Navbar title="My App" about="About" mode={mode} toggleMode={toggleMode} lable={toggleName}/>
     {/* <Navbar /> */}
     <div className="container">
-    <HomeForm heading="Enter Your Text" label="Enter your Text" />
+        <HomeForm heading="Enter Your Text" mode={mode}  label="Enter your Text" />
     {/* <About/> */}
     </div>
     </>
--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -33,26 +33,26 @@ export default function HomeForm(props) {
     }
     const [text,setText]= useState('');
   return (
-    <div>
+      <div className='container' style={{ color: props.mode === 'dark' ? 'white' : '#042743'}}>
         <h1>{props.heading}</h1>
           {/* <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
               <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
           </div> */}
           <div className="mb-3">
-              <textarea className="form-control" value={text} onChange={handleOnchange} placeholder="Enter your text" id="myBox" row="1"></textarea>
+              <textarea className="form-control" value={text} onChange={handleOnchange} placeholder="Enter your text" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" row="1"></textarea>
           </div>
           <button className="btn btn-primary mx-3 my-3" onClick={handleUpClick}>Upper Case</button>
           <button className="btn btn-primary mx-3 my 3" onClick={handleLoClick}>Lower Case</button>
           <button className="btn btn-primary mx-3 my-3" onClick={handleClearClick}>Clear</button>
           <button className="btn btn-primary mx-3 my-3" onClick={handleCopy}>Copy</button>
           <button className="btn btn-primary mx-3 my-3" onClick={handleExtraSpaces}>Remove Extra Space</button>
-          <div className="container my-3">
+          <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
               <h1>Your text summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes read (Average Reading Speed)</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
           </div>
     </div>
   )   
