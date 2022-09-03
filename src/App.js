0104c650bb8fbfc5aa5041a81bc20beaf616ed62
Tushar Sharma <108931258+NewCodeTushar@users.noreply.github.com>
2022-09-03 21:31:33 +0530

    Deploying single package

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Navbar from './components/Navbar';
 import HomeForm from './components/HomeForm';
 import Alert from './components/Alert';
 import About from './components/About';
-import{
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+// import{
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -42,17 +42,18 @@ function App() {
   }
   return (
     <>
-  <Router>
+  {/* <Router> */}
       <Navbar title="My App" about="About" mode={mode} toggleMode={toggleMode} lable={toggleName}/>
       <Alert alert={alert}/>
     {/* <Navbar /> */}
     <div className="container">
-      <Routes>
-            <Route exact path="/about" element={<About/>}/>         
-            <Route exact path="/" element={<HomeForm showAlert={showAlert} heading="Enter Your Text" mode={mode} label="Enter your Text" />}/>
-      </Routes>
+      {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About/>}/>          */}
+            {/* <Route exact path="/" element={<HomeForm showAlert={showAlert} heading="Enter Your Text" mode={mode} label="Enter your Text" />}/>
+      </Routes> */}
+      <HomeForm showAlert={showAlert} heading="Enter Your Text" mode={mode} label="Enter your Text" />
     </div>
-  </Router>
+  {/* </Router> */}
     </>
   );
 }
