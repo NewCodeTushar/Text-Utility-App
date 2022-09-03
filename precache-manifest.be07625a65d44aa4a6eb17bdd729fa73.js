6261cc0d9902220cff31ce9b4da81ac237edf69e
Tushar Sharma <108931258+NewCodeTushar@users.noreply.github.com>
2022-09-03 21:37:49 +0530

    Updates

---
+++ precache-manifest.be07625a65d44aa4a6eb17bdd729fa73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "620d6f422959b8562a91",
+    "url": "/Text-Utility-App/static/css/main.51f03fcf.chunk.css"
+  },
+  {
+    "revision": "620d6f422959b8562a91",
+    "url": "/Text-Utility-App/static/js/main.620d6f42.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Utility-App/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Utility-App/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "25167924bad121eaaf69",
+    "url": "/Text-Utility-App/static/js/runtime~main.25167924.js"
+  },
+  {
+    "revision": "bb59b9eeef3836cea44d591439a1cf95",
+    "url": "/Text-Utility-App/index.html"
+  }
+];
